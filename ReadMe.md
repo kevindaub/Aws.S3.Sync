7195c57acb0d11cc3b7de17f5de388f7378e2b03
Kevin Daub <daub815@gmail.com>
2017-05-29 18:13:03 -0400

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,34 @@
+# Aws.S3.Sync
+
+Small wrapper script that calls AWS S3 synchronization and emails the results at the end.
+
+## TODO
+
+[ ] - Store encryption key securely.
+[ ] - Store email password securely.
+[ ] - Sign script
+[ ] - Create chocolatey package.
+
+## Preqs
+
+- [AWS PowerShell Tools](http://docs.aws.amazon.com/powershell/latest/userguide/pstools-getting-set-up.html)
+
+## Usage
+
+```powershell
+$remote = "s3://bucketname/"
+$local = "C:\users\"
+$users = @("User1","User2")
+$encryptionKey = "0000000-0000-0000-0000-000000000000"
+$username = "me@kevindaub.com"
+$password = "a_secret"
+$logsdir = "C:\sync\logs\"
+$dryrun = $false
+
+$scriptdir = Split-Path $script:MyInvocation.MyCommand.Path
+. "$scriptdir\SyncUsers.ps1" -remote $remote -local $local -users $users -encryptionKey $encryptionKey -logsdir $logsdir -username $username -password $password -dryrun:$dryrun
+```
+
+```dos
+powershell.exe -executionpolicy bypass C:\example_above.ps1
+```
---
+++ Sync-Users.ps1
@@ -0,0 +1,119 @@
+﻿[CmdletBinding()]
+    Param(
+        [string]$remote,
+        [string]$local,
+        [string[]]$users,
+        [string]$encryptionKey,
+        [string]$logsdir,
+        [string]$username,
+        [string]$password,
+        [switch]$dryrun,
+        [switch]$donotexecute,
+    )
+
+function Archive-Logs
+{
+    [CmdletBinding()]
+    Param(
+        [string]$logsdir
+    )
+
+    if (!(Test-Path $logsdir)) {
+        New-Item $logsdir -ItemType Directory -Force | Out-Null
+    }
+
+    $logs = Get-ChildItem $logsdir -Filter "*.log"
+    if ($logs -ne $null) {
+        $archivedir = "$logsdir$(Get-Date -format "dd-MM-yyyyTHH.mm.s")"
+        Write-Output "Archive Dir $archivedir"
+        foreach ($log in $logs) {
+            New-Item $archivedir -ItemType Directory -Force | Out-Null
+            Move-Item $log.FullName "$archivedir" -Force | Out-Null
+        }
+    }
+}
+
+function Email-Results
+{
+    [CmdletBinding()]
+    Param(
+        [string]$smtp = "smtp.gmail.com",
+        [int]$smtpport = 587,
+        [string]$username,
+        [string]$password,
+        [string]$subject,
+        [string]$body,
+        [System.Net.Mail.Attachment[]]$attachments
+    )
+
+    $message = New-Object Net.Mail.MailMessage($username, $username, $subject, $body)
+    $attachments | ForEach { $message.Attachments.Add($_) }
+    $SMTPClient = New-Object Net.Mail.SmtpClient($smtp, $smtpport)
+    $SMTPClient.EnableSsl = $true
+    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($username, $password);
+    $SMTPClient.Send($message)
+}
+
+function Sync-Users
+{
+    [CmdletBinding()]
+    Param(
+        [string]$remote,
+        [string]$local,
+        [string[]]$users,
+        [string[]]$localdirs = @("Documents", "Desktop")
+        [string]$encryptionKey,
+        [string]$logsdir,
+        [switch]$dryrun
+    )
+
+    $start = Get-Date
+    Write-Output "Start Sync at $start"
+
+    $dryrunCmd = ""
+    if ($dryrun.ToBool()) {
+        $dryrunCmd = "--dryrun"
+    }
+
+    foreach ($user in $users) {
+        $startUser = Get-Date
+        Write-Output "Syncing the user of $user at $startUser"
+        $command = "aws"
+
+        foreach ($localdir in $localdirs) {
+            $args = "s3 sync $local$user\$localdir\ $remote$user/$localdir/ --delete --sse `"aws:kms`" --sse-kms-key-id `"$encryptionKey`" $dryrunCmd"
+            Write-Output "$command $args"
+			$log = "$logsDir$user-$localdir.log"
+            Start-Process $command -ArgumentList $args -NoNewWindow -Wait -RedirectStandardOutput $log
+        }
+
+        if (Test-Path $log) {
+            Get-Content $log | Write-Output
+        } else {
+            Write-Warning "This nominally means nothing needed to be synced"
+        }
+
+        $stopUser = Get-Date
+        Write-Output "Finished syncing the user of $user at $stopUser"
+    }
+
+    $stop = Get-Date
+    Write-Output "Finished Sync at $stop"
+}
+
+if ($donotexecute.ToBool())
+{
+    $scriptdir = Split-Path $script:MyInvocation.MyCommand.Path
+    . "$scriptdir\Sync-Users.ps1"
+
+    Archive-Logs -logsdir $logsdir
+
+    $start = Get-Date
+
+    Sync-Users -remote $remote -local $local -users $users -encryptionKey $encryptionKey -logsdir $logsdir -dryrun:$dryrun
+
+    $logs = Get-ChildItem $logsdir -Filter "*.log"
+    $stop = Get-Date
+    $attachments = $logs | ForEach { new-object System.Net.Mail.Attachment $_.FullName }
+    Email-Results -username $username -password $password -subject "Computer Sync Completed" -body "Computer Sync Completed in '$($stop - $start)' at $stop" -attachments $attachments
+}
